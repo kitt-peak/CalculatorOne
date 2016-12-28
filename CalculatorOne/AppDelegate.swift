c70b9cca319cc98b20f9ed0d68650356ce5e7b0f
Andreas Günther <kitt.peak@arcor.de>
2016-12-28 14:25:45 +0100

    Initial Commit

---
+++ CalculatorOne/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  CalculatorOne
+//
+//  Created by Andreas on 28/12/2016.
+//  Copyright © 2016 Kitt Peak. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
